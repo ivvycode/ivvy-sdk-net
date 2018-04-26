8b5cdb2885ecba24655d488c636f182bd27af3c7
David Tannock <david.tannock@ivvy.com>
2018-04-26 11:58:38 +1000

    Added the Room and RatePlan venue objects. Added the GetVenueRoomListAsync and GetVenueRatePlanListAsync api methods.

---
+++ src/Venue/RatePlan.cs
@@ -0,0 +1,24 @@
+using System;
+using Newtonsoft.Json;
+using Ivvy.Json;
+
+namespace Ivvy.Venue
+{
+    /// <summary>
+    /// An iVvy venue rate plan.
+    /// </summary>
+    public class RatePlan : ISerializable
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+}
