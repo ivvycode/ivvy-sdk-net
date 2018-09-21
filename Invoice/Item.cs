ec4bb8e4d0b53f10079c95d984901460dcebba77
Vijay.Patel <vijay.patel@cygnetinfotech.com>
2018-09-21 16:48:45 +0530

    AE-647 iVvy SDK Change for iVvy RefType
    - Added RefType enum for Invoice Item
    - Invoice Item string RefType to enum RefType

--- Invoice/Item.cs
+++ Invoice/Item.cs
@@ -4,8 +4,29 @@ namespace Ivvy.Invoice
 {
     public class Item
     {
+        public enum RefTypes
+        {
+            Unknown = 0,
+            Fee = 100,
+            CardFee = 102,
+            PaymentFee = 103,
+            Deposit = 500,
+            Package = 501,
+            Invoice = 502,
+            Payment = 503,
+            FinalInvoiceAdjustment = 504,
+            Session = 505,
+            MenuPackage = 506,
+            BeveragePackage = 507,
+            Resource = 508,
+            Product = 509,
+            Accommodation = 510,
+            AdditionalItem = 511,
+            ServiceFee = 513
+        }
+
         [JsonProperty("refType")]
-        public string RefType { get; set; }
+        public RefTypes RefType { get; set; }
 
         [JsonProperty("description")]
         public string Description { get; set; }
