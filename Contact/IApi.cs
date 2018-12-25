b08379f203a26cff115e40f591cdd4bab702d85c
Vijay.Patel <vijay.patel@cygnetinfotech.com>
2018-12-25 15:09:48 +0530

    - Added GetCompanyListAsync method with filter parameter
    - Added AddOrUpdateCompanyAsync method with post data
    - Added modified date to Company model

--- Contact/IApi.cs
+++ Contact/IApi.cs
@@ -34,5 +34,21 @@ namespace Ivvy
         /// <param name="id">The identifier.</param>
         /// <returns></returns>
         Task<ResultOrError<Contact.Company>> GetCompanyAsync(int id);
+
+        /// <summary>
+        /// Returns a collection of companies.
+        /// </summary>
+        /// <param name="perPage">The per page.</param>
+        /// <param name="start">The start.</param>
+        /// <param name="filterRequest">The filter request.</param>
+        /// <returns></returns>
+        Task<ResultOrError<ResultList<Contact.Company>>> GetCompanyListAsync(int perPage, int start, Dictionary<string, object> filterRequest);
+
+        /// <summary>
+        /// Adds or updates a company.
+        /// </summary>
+        /// <param name="company">The company.</param>
+        /// <returns></returns>
+        Task<ResultOrError<ResultObject>> AddOrUpdateCompanyAsync(Contact.Company company);
     }
 }
