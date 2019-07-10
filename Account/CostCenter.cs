47563cc1cbcfe208a038d2adaf2f2156e6f91b3c
Mihail Sotirov <mihail.sotirov@sciant.com>
2019-07-10 10:22:43 +0300

    Added CostCenter.

---
+++ Account/CostCenter.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace Ivvy.Account
+{
+    public class CostCenter
+    {
+        [JsonProperty("id")]
+        public int? Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("defaultType")]
+        public string DefaultType { get; set; }
+    }
+}
