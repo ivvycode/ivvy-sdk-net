868fb9bcfaca325669cb142b2bcb410f9147dcc9
David Tannock <david.tannock@ivvy.com>
2020-03-18 18:04:51 +1000

    Added the int value to the NoteType enum.

--- Venue/Note.cs
+++ Venue/Note.cs
@@ -8,6 +8,46 @@ namespace Ivvy.Venue
     /// </summary>
     public class Note
     {
+        public enum NoteType
+        {
+            LeadOriginal = -1,
+            NoType = 0,
+            Booking = 1,
+            Session = 2,
+            Billing = 3,
+            SessionMenu = 4,
+            SessionBeverage = 5,
+            SessionResourses = 6,
+            SessionProducts = 7,
+            Accommodation = 8,
+            Contact = 9,
+            Company = 10,
+            Couriers = 11,
+            VipGuests = 12,
+            Security = 13,
+            OnSideContractors = 14,
+            Signatories = 15,
+            FrontOffices = 16,
+            Transfers = 17,
+            RoomDrops = 18,
+            LuggageServices = 19,
+            GroupProfile = 20,
+            Lead = 21,
+            DietaryRequirements = 22,
+            AllDepartments = 23,
+            FoodAndBeverage = 24,
+            CarParking = 25,
+            Suppliers = 26,
+            Internet = 27,
+            LeadDescription = 28,
+            Signage = 29,
+            SalesOffice = 30,
+            BellmanVanService = 31,
+            Reservation = 32,
+            Housekeeping = 33,
+            Engineering = 34
+        }
+
         [JsonProperty("id")]
         public int Id { get; set; }
 
@@ -34,45 +74,5 @@ namespace Ivvy.Venue
 
         [JsonProperty("modifiedDate")]
         public DateTime ModifiedDate { get; set; }
-
-        public enum NoteType
-        {
-            LeadOriginal = -1,
-            NoType,
-            Booking,
-            Session,
-            Billing,
-            SessionMenu,
-            SessionBeverage,
-            SessionResourses,
-            SessionProducts,
-            Accommodation,
-            Contact,
-            Company,
-            Couriers,
-            VipGuests,
-            Security,
-            OnSideContractors,
-            Signatories,
-            FrontOffices,
-            Transfers,
-            RoomDrops,
-            LuggageServices,
-            GroupProfile,
-            Lead,
-            DietaryRequirements,
-            AllDepartments,
-            FoodAndBeverage,
-            CarParking,
-            Suppliers,
-            Internet,
-            LeadDescription,
-            Signage,
-            SalesOffice,
-            BellmanVanService,
-            Reservation,
-            Housekeeping,
-            Engineering
-        }
     }
 }
