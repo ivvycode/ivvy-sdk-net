f97fcf743cae7c23af47d313c36564f4e99171bc
David Tannock <david.tannock@ivvy.com>
2020-05-30 13:44:12 +1000

    Fixed the test namespace.

--- tests/PlatformExtensions/Setup/VerifySetupServiceTests.cs
+++ tests/PlatformExtensions/Setup/VerifySetupServiceTests.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace NslCrm.Tests.IvvyApi.Extensions
+namespace Ivvy.Tests.PlatformExtensions.Setup
 {
     public class VerifySetupServiceTests
     {
