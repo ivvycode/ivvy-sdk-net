520c1afc4699022571ac51a7a1459f0cb7e84684
mihail.sotirov@sciant.com <mihail.sotirov@sciant.com>
2020-10-26 10:35:02 +0200

    Added an ID to the invoice line item.

--- src/Invoice/Item.cs
+++ src/Invoice/Item.cs
@@ -25,6 +25,12 @@ namespace Ivvy.API.Invoice
             ServiceFee = 513
         }
 
+        [JsonProperty("id")]
+        public int Id
+        {
+            get; set;
+        }
+
         [JsonProperty("refType")]
         public int RefType
         {
