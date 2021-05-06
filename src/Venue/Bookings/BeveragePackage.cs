a0aa83051e27592537c012664519c05bc5bd2c86
Haaris Ahmad <haaris.ahmad@ivvy.com>
2021-05-06 14:53:38 +1000

    AE-1932: added costCentreId to booking items

--- src/Venue/Bookings/BeveragePackage.cs
+++ src/Venue/Bookings/BeveragePackage.cs
@@ -69,6 +69,12 @@ namespace Ivvy.API.Venue.Bookings
             get; set;
         }
 
+        [JsonProperty("costCenterId")]
+        public int CostCenterId
+        {
+            get; set;
+        }
+
         [JsonProperty("marketplaceName")]
         public string MarketplaceName
         {
--- src/Venue/Bookings/Resource.cs
+++ src/Venue/Bookings/Resource.cs
@@ -51,6 +51,12 @@ namespace Ivvy.API.Venue.Bookings
             get; set;
         }
 
+        [JsonProperty("costCenterId")]
+        public int CostCenterId
+        {
+            get; set;
+        }
+
         [JsonProperty("totalAmount")]
         public decimal TotalAmount
         {
