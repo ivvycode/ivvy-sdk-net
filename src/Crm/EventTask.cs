efa7a259f60d33e0e7ddc36504e9608cf61e7dbf
Haaris Ahmad <haaris.ahmad@ivvy.com>
2021-05-27 17:26:24 +1000

    AE-1941: Added eventtask entity

---
+++ src/Crm/EventTask.cs
@@ -0,0 +1,130 @@
+using System;
+using Ivvy.API.Json;
+using Newtonsoft.Json;
+
+namespace Ivvy.API.Crm
+{
+    /// <summary>
+    /// An iVvy CRM event task.
+    /// </summary>
+    public class EventTask : ISerializable
+    {
+        public enum EventStatusOptions
+        {
+            NotStarted = 0,
+            InProgress = 1,
+            OnHold = 2,
+            Cancelled = 3,
+            Completed = 4
+        }
+
+
+        [JsonProperty("id")]
+        public string Id
+        {
+            get; set;
+        }
+
+        [JsonProperty("venueId")]
+        public string VenueId
+        {
+            get; set;
+        }
+
+        [JsonProperty("leadId")]
+        public string LeadId
+        {
+            get; set;
+        }
+
+        [JsonProperty("name")]
+        public string Name
+        {
+            get; set;
+        }
+
+        [JsonProperty("description")]
+        public string Description
+        {
+            get; set;
+        }
+
+        [JsonProperty("status")]
+        public EventStatusOptions Status
+        {
+            get; set;
+        }
+
+        [JsonProperty("priority")]
+        public string Priority
+        {
+            get; set;
+        }
+
+        [JsonProperty("contactId")]
+        public string ContactId
+        {
+            get; set;
+        }
+
+        [JsonProperty("companyId")]
+        public string CompanyId
+        {
+            get; set;
+        }
+
+        [JsonProperty("companyContactId")]
+        public string CompanyContactId
+        {
+            get; set;
+        }
+
+        [JsonProperty("assignedUserId")]
+        public string AssignedUserId
+        {
+            get; set;
+        }
+
+        [JsonProperty("assignedUserFullName")]
+        public string AssignedUserFullName
+        {
+            get; set;
+        }
+
+        [JsonProperty("assignedUserEmail")]
+        public string AssignedUserEmail
+        {
+            get; set;
+        }
+
+        [JsonProperty("endDatetime")]
+        public string EndDatetime
+        {
+            get; set;
+        }
+
+        [JsonProperty("startDatetime")]
+        public string StartDatetime
+        {
+            get; set;
+        }
+
+        [JsonProperty("progress")]
+        public string Progress
+        {
+            get; set;
+        }
+
+        [JsonProperty("createdDate")]
+        public DateTime? CreatedDate
+        {
+            get; set;
+        }
+
+        [JsonProperty("modifiedDate")]
+        public DateTime? ModifiedDate
+        {
+            get; set;
+        }
+    }
+}
