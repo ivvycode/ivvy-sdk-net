ac965e0044d8ac82b7bf6eb44e65c34f1ca536d2
jenny.kowald@ivvy.com <jenny.kowald@ivvy.com>
2021-08-06 10:56:48 +1000

    Ensure that the correct spelling of center/centre is used.

--- src/Venue/Bookings/CostCentreCommission.cs
+++ src/Venue/Bookings/CostCentreCommission.cs
@@ -5,14 +5,21 @@ using Newtonsoft.Json;
 namespace Ivvy.API.Venue.Bookings
 {
     /// <summary>
-    /// Details of a cost centre commission in a venue booking.
+    /// Details of a cost center commission in a venue booking.
     /// </summary>
     public class CostCentreCommission : ISerializable
     {
-        [JsonProperty("costCentreId")]
-        public int CostCentreId
+        [JsonProperty("costCenterId")]
+        public int CostCenterId
         {
             get; set;
+        }
+
+        [Obsolete("This property has been deprecated. Use CostCenterId instead.")]
+        public int CostCentreId
+        {
+            get => CostCenterId;
+            set => CostCenterId = value;
         }
 
         [JsonProperty("commission")]
