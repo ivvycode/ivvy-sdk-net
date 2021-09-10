9bd65323db51a995b10206a960720d65fb286ebd
jenny.kowald@ivvy.com <jenny.kowald@ivvy.com>
2021-09-10 11:55:52 +1000

    Refactor the Venue level cost centre to inherit from the Account level cost centre to allow sharing of data structures.

--- src/Venue/CostCenter.cs
+++ src/Venue/CostCenter.cs
@@ -2,48 +2,8 @@ using Newtonsoft.Json;
 
 namespace Ivvy.API.Venue
 {
-    public class CostCenter
+    public class CostCenter : Account.CostCenter
     {
-        public enum DefaultTypes
-        {
-            Other = 1,
-            VenueFood = 2,
-            VenueBeverage = 3,
-            VenueAudioVisual = 4,
-            VenueRoomHire = 5,
-            VenueAccommodation = 6,
-        }
-
-        [JsonProperty("id")]
-        public int? Id
-        {
-            get; set;
-        }
-
-        [JsonProperty("name")]
-        public string Name
-        {
-            get; set;
-        }
-
-        [JsonProperty("code")]
-        public string Code
-        {
-            get; set;
-        }
-
-        [JsonProperty("description")]
-        public string Description
-        {
-            get; set;
-        }
-
-        [JsonProperty("defaultType")]
-        public DefaultTypes? DefaultType
-        {
-            get; set;
-        }
-
         [JsonProperty("parentId")]
         public int? ParentId
         {
