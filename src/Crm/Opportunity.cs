69e734531beeb07c30d83ee1cfff9f20e88c7d0c
Dinesh Duraisamy <dinesh.duraisamy@ivvy.com>
2021-10-29 11:01:36 +1000

    Added lead class and updated opportunity in crm.

--- src/Crm/Opportunity.cs
+++ src/Crm/Opportunity.cs
@@ -1,5 +1,4 @@
 using System;
-using Ivvy.API.Json;
 using Newtonsoft.Json;
 
 namespace Ivvy.API.Crm
@@ -7,94 +6,10 @@ namespace Ivvy.API.Crm
     /// <summary>
     /// An iVvy CRM opportunity.
     /// </summary>
-    public class Opportunity
+    public class Opportunity : Lead
     {
-        [JsonProperty("id", DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int Id
-        {
-            get; set;
-        }
-
         [JsonProperty("venueId")]
         public int VenueId
-        {
-            get; set;
-        }
-
-        [JsonProperty("name")]
-        public string Name
-        {
-            get; set;
-        }
-
-        [JsonProperty("description")]
-        public string Description
-        {
-            get; set;
-        }
-
-        [JsonProperty("companyId")]
-        public int? CompanyId
-        {
-            get; set;
-        }
-
-        [JsonProperty("companyLeadContactId")]
-        public int? CompanyLeadContactId
-        {
-            get; set;
-        }
-
-        [JsonProperty("contactId")]
-        public int? ContactId
-        {
-            get; set;
-        }
-
-        [JsonProperty("qualityId")]
-        public int? QualityId
-        {
-            get; set;
-        }
-
-        [JsonProperty("industryId")]
-        public int? IndustryId
-        {
-            get; set;
-        }
-
-        [JsonProperty("sourceId")]
-        public int? SourceId
-        {
-            get; set;
-        }
-
-        [JsonProperty("ownerUserId")]
-        public int? OwnerUserId
-        {
-            get; set;
-        }
-
-        [JsonProperty("typeId")]
-        public int? TypeId
-        {
-            get; set;
-        }
-
-        [JsonProperty("stageId")]
-        public int? StageId
-        {
-            get; set;
-        }
-
-        [JsonProperty("stageReasonId")]
-        public int? StageReasonId
-        {
-            get; set;
-        }
-
-        [JsonProperty("channelId")]
-        public int? ChannelId
         {
             get; set;
         }
@@ -182,35 +97,5 @@ namespace Ivvy.API.Crm
         {
             get; set;
         }
-
-        [JsonProperty("customFields")]
-        public CustomField CustomFields
-        {
-            get; set;
-        }
-      
-        [JsonProperty("ownerUser")]
-        public OpportunityOwner OwnerUser
-        {
-            get; set;
-        }
-    }
-
-    public class CustomField : ISerializable
-    {
-        [JsonProperty("fieldId")]
-        public int FieldId
-        {
-            get; set;
-        }
-
-
-        [JsonProperty("fieldValue")]
-        public object FieldValue
-        {
-            get; set;
-        }
-
-
     }
 }
