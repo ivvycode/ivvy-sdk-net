e9c80dda970b926699a56d1d142824364daf3784
MihailSotirov <43601945+MihailSotirov@users.noreply.github.com>
2021-11-10 00:25:22 +0200

    AE-2161-exposing accommodation reservation methods (#82)
    * Added accommodation reservation methods to Booking data model.
    
    * Added some unit tests to ensure the enum collection is being deserialized as expected by the response converter.

---
+++ tests/Venue/BookingTests.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using Ivvy.API.Json.Converters;
+using Ivvy.API.Venue;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Xunit;
+
+namespace Ivvy.API.UnitTests.Venue
+{
+    public class BookingTests
+    {
+        [Fact]
+        public void Booking_WithoutReservationMethods_DeserializedCorrectly()
+        {
+            var booking = new Booking
+            {
+                Id = 42
+            };
+            var bookingAsString = this.SerializeBooking(booking);
+            var response = this.GetBookingResultOrError(bookingAsString);
+
+            Assert.Null(response.Result.AccommodationReservationMethods);
+        }
+
+        [Fact]
+        public void Booking_WithEmptyReservationMethods_DeserializedCorrectly()
+        {
+            var booking = new Booking
+            {
+                Id = 42,
+                AccommodationReservationMethods =
+                    Array.Empty<Booking.AccommodationReservationMethodOptions>()
+            };
+            var bookingAsString = this.SerializeBooking(booking);
+            var response = this.GetBookingResultOrError(bookingAsString);
+
+            Assert.Empty(response.Result.AccommodationReservationMethods);
+        }
+
+        [Fact]
+        public void Booking_WithReservationMethods_DeserializedCorrectly()
+        {
+            var booking = new Booking
+            {
+                Id = 42,
+                AccommodationReservationMethods =
+                    new Booking.AccommodationReservationMethodOptions[]
+                    {
+                        Booking.AccommodationReservationMethodOptions.RoomingList,
+                        Booking.AccommodationReservationMethodOptions.BookingEngine,
+                    }
+            };
+            var bookingAsString = this.SerializeBooking(booking);
+            var response = this.GetBookingResultOrError(bookingAsString);
+
+            Assert.Contains(
+                response.Result.AccommodationReservationMethods,
+                rm => rm == Booking.AccommodationReservationMethodOptions.RoomingList);
+
+            Assert.Contains(
+                response.Result.AccommodationReservationMethods,
+                rm => rm == Booking.AccommodationReservationMethodOptions.BookingEngine);
+        }
+
+        private ResultOrError<Booking> GetBookingResultOrError(string bookingAsString)
+        {
+            return JsonConvert.DeserializeObject<ResultOrError<Booking>>(
+                bookingAsString, new ResponseConverter<Booking>());
+        }
+
+        private string SerializeBooking(Booking booking)
+        {
+            return JsonConvert.SerializeObject(
+                booking,
+                Formatting.None,
+                new JsonSerializerSettings
+                {
+                    NullValueHandling = NullValueHandling.Ignore,
+                    Converters = new List<JsonConverter>()
+                    {
+                        new IsoDateTimeConverter()
+                        {
+                            DateTimeFormat = Utils.DateTimeFormat
+                        }
+                    }
+                });
+        }
+    }
+}
